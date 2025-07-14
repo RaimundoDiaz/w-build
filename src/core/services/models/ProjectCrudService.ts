dd0f33e2831dc16e9cd86765b851d5bc5abda136
RaimundoDiaz <rediazparada@gmail.com>
2025-07-13 21:51:21 -0400

    Add ProjectCrudService class for managing project-related API interactions, including methods to list all projects and retrieve a project by ID.

---
+++ src/core/services/models/ProjectCrudService.ts
@@ -0,0 +1,22 @@
+import { Project } from "@/core/entities/Project.entity";
+import { AppResponse } from "@/core/types/makeRequest.types";
+import makeRequest from "@/core/utils/makeRequest";
+import { HttpMethodType } from "@/presentation/enums/HttpMethodType";
+
+const BASE_API_URL = "/api/models/project";
+
+export default abstract class ProjectCrudService {
+  /**
+   * @param params { }. Will fetch all projects.
+   */
+  public static async list(): Promise<AppResponse<Project[]>> {
+    return await makeRequest<Project[]>(BASE_API_URL, HttpMethodType.GET);
+  }
+
+  public static async get(id: string): Promise<AppResponse<Project>> {
+    return await makeRequest<Project>(
+      [BASE_API_URL, id].join("/"),
+      HttpMethodType.GET
+    );
+  }
+}
