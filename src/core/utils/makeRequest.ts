4633bfc533e942bea686cbc19db99159f5712317
RaimundoDiaz <rediazparada@gmail.com>
2025-07-13 20:44:18 -0400

    Add makeRequest utility function for simplified API requests with support for various HTTP methods

---
+++ src/core/utils/makeRequest.ts
@@ -0,0 +1,24 @@
+import { AppResponse } from "@/core/types/makeRequest.types";
+import { HttpMethodType } from "@/presentation/enums/HttpMethodType";
+
+const makeRequest = async <T>(
+  url: string,
+  method: HttpMethodType,
+  data?: unknown
+): Promise<AppResponse<T>> => {
+  const fetchBodyParams =
+    data !== undefined
+      ? {
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
+        }
+      : {};
+
+  const response = await fetch(url, { method, ...fetchBodyParams });
+
+  return new AppResponse<T>(response);
+};
+
+export default makeRequest;
