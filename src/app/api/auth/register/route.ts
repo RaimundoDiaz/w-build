aa67d0b65c144beabd87648467506fee9d750f18
RaimundoDiaz <rediazparada@gmail.com>
2025-07-13 20:43:28 -0400

    Add POST route for user registration with validation using Zod

---
+++ src/app/api/auth/register/route.ts
@@ -0,0 +1,21 @@
+import { AuthenticationUseCase } from "@/core/useCases/authentication/Authentication.useCase";
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+
+export const POST = async (request: NextRequest): Promise<Response> => {
+  const permittedParameters = z.object({
+    email: z.string().trim().nonempty().email(),
+    plainPassword: z.string().trim().nonempty(),
+  });
+
+  const params = permittedParameters.parse(await request.json());
+  await AuthenticationUseCase.register({
+    email: params.email,
+    plainPassword: params.plainPassword,
+  });
+
+  return NextResponse.json(
+    { message: "User registered successfully" },
+    { status: 201 }
+  );
+};
