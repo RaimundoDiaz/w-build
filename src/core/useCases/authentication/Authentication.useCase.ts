0d614ffa99e3e849b169e42dfbe621ae86bbe771
RaimundoDiaz <rediazparada@gmail.com>
2025-07-13 20:42:22 -0400

    Add AuthenticationUseCase class for user registration logic

---
+++ src/core/useCases/authentication/Authentication.useCase.ts
@@ -0,0 +1,13 @@
+import { AuthenticationRepository } from "@/infrastructure/repositories/authentication/Authentication.repository";
+
+export abstract class AuthenticationUseCase {
+  public static async register({
+    email,
+    plainPassword,
+  }: {
+    email: string;
+    plainPassword: string;
+  }): Promise<void> {
+    await AuthenticationRepository.register({ email, plainPassword });
+  }
+}
