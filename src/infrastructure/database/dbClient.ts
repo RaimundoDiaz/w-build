68ae57ab19964080921abab6a6f985858ecef49f
RaimundoDiaz <rediazparada@gmail.com>
2025-07-13 20:40:54 -0400

    Update database client connection string to use DATABASE_URL environment variable

--- src/infrastructure/database/dbClient.ts
+++ src/infrastructure/database/dbClient.ts
@@ -4,7 +4,7 @@ import { Pool } from "pg";
 import * as schema from "./schemas";
 
 const client = new Pool({
-  connectionString: process.env.DB_POSTGRES_CONNECTION_URI!
+  connectionString: process.env.DATABASE_URL!,
 });
 
 const db = drizzle(client, { schema, logger: true });
