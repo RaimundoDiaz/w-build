3148f332140361f125d09b9bc97b808b5be1378f
RaimundoDiaz <rediazparada@gmail.com>
2025-07-13 20:47:05 -0400

    Add AuthContextProvider component for managing authentication context with NextAuth session support

---
+++ src/presentation/context/AuthContext.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { Session } from "next-auth";
+import { SessionProvider } from "next-auth/react";
+import React from "react";
+
+const AuthContextProvider = ({
+  children,
+  session
+}: {
+  children: React.ReactNode
+  session: Session | null
+}) => {
+
+  return (
+    <SessionProvider session={session}>
+      {children}
+    </SessionProvider>
+  );
+};
+
+export default AuthContextProvider;
