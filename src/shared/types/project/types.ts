2c224c2e75b9b5c2dc8770126c10ef64abf588c0
RaimundoDiaz <rediazparada@gmail.com>
2025-07-16 14:02:24 -0400

    Update ProjectCreateParams type to include id and createdAt fields, ensuring comprehensive project data representation.

--- src/shared/types/project/types.ts
+++ src/shared/types/project/types.ts
@@ -1,7 +1,14 @@
 export type ProjectCreateParams = {
+  id: string;
+  createdAt: Date;
   name: string;
   description: string | null | undefined;
   imageUrl: string | null | undefined;
-  id: string;
-  createdAt: Date;
+  location: string;
+  targetAmount: number;
+  currentAmount: number;
+  minInvestment: number;
+  expectedReturn: string;
+  investors: number;
+  status: string;
 };
