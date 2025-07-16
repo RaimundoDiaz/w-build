dd6dc226575905c3488bcd9da0701ed2a580a51a
RaimundoDiaz <rediazparada@gmail.com>
2025-07-16 14:00:34 -0400

    Enhance project schema by adding new fields: location, targetAmount, currentAmount, minInvestment, expectedReturn, investors, and status for improved data structure and integrity.

--- src/infrastructure/database/schemas/Project.schema.ts
+++ src/infrastructure/database/schemas/Project.schema.ts
@@ -1,10 +1,17 @@
-import { pgTable, varchar } from "drizzle-orm/pg-core";
+import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
 
 import { baseModelColumns } from "./common/baseModelColumns";
 
 export const projectSchema = pgTable("project", {
   ...baseModelColumns,
-  name:        varchar("name", { length: 256 }).notNull(),
-  description: varchar("description", { length: 512 }),
-  imageUrl:    varchar("image_url", { length: 512 })
+  name:           varchar("name", { length: 256 }).notNull(),
+  description:    varchar("description", { length: 512 }),
+  imageUrl:       varchar("image_url", { length: 512 }),
+  location:       varchar("location", { length: 256 }).notNull(),
+  targetAmount:   integer("target_amount").notNull(),
+  currentAmount:  integer("current_amount").notNull(),
+  minInvestment:  integer("min_investment").notNull(),
+  expectedReturn: varchar("expected_return", { length: 256 }).notNull(),
+  investors:      integer("investors").notNull(),
+  status:         varchar("status", { length: 256 }).notNull()
 });
