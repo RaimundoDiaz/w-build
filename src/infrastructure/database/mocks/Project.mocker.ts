82985c431b2195f76cb14294e5673119f137eb14
RaimundoDiaz <rediazparada@gmail.com>
2025-07-16 19:31:46 -0400

    Update ProjectMocker to generate dynamic targetAmount and currentAmount values, enhancing the realism of mock project data for testing.

--- src/infrastructure/database/mocks/Project.mocker.ts
+++ src/infrastructure/database/mocks/Project.mocker.ts
@@ -12,6 +12,8 @@ export class ProjectMocker extends BaseMocker {
   public static async generate(amount = 20): Promise<void> {
     const projects = [];
     for (let i = 0; i < amount; i++) {
+      const targetAmount = faker.number.int({ min: 1000000, max: 10000000 });
+      const currentAmount = faker.number.int({ min: 100000, max: targetAmount });
       projects.push({
         id:             faker.string.uuid(),
         createdAt:      faker.date.past(),
@@ -19,12 +21,12 @@ export class ProjectMocker extends BaseMocker {
         description:    faker.lorem.sentence(),
         imageUrl:       faker.image.url({ width: 500, height: 500 }),
         location:       faker.location.city() + ", " + faker.location.state() + ", " + faker.location.country(),
-        targetAmount:   faker.number.int({ min: 1000000, max: 10000000 }),
-        currentAmount:  faker.number.int({ min: 1000000, max: 10000000 }),
+        targetAmount,
+        currentAmount,
         minInvestment:  faker.number.int({ min: 10000, max: 1000000 }),
         expectedReturn: faker.helpers.arrayElement(["4-6%", "10-12%", "14-16%", "18-20%"]),
-        investors:      faker.number.int({ min: 10, max: 100 }),
-        status:         faker.helpers.arrayElement(["active", "inactive"])
+        investors:      faker.number.int({ min: 10, max: 200 }),
+        status:         faker.helpers.arrayElement(["active"])
       });
     }
     await db.insert(projectSchema).values(projects);
