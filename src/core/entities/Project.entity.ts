d4003438965761e0e9a270f65c3393f01a33a3f5
RaimundoDiaz <rediazparada@gmail.com>
2025-07-16 14:01:30 -0400

    Enhance Project entity by adding new properties (location, targetAmount, currentAmount, minInvestment, expectedReturn, investors, status) and corresponding getters for improved data handling and integrity.

--- src/core/entities/Project.entity.ts
+++ src/core/entities/Project.entity.ts
@@ -5,9 +5,16 @@ import { baseModelZodAttributes } from "./common/baseModelZodAttributes";
 
 const zodSchema = z.object({
   ...baseModelZodAttributes,
-  name:        z.string().min(1),
-  description: z.string().nullable().default(null),
-  imageUrl:    z.string().nullable().default(null)
+  name:           z.string().min(1),
+  description:    z.string().nullable().default(null),
+  imageUrl:       z.string().nullable().default(null),
+  location:       z.string().min(1),
+  targetAmount:   z.number().min(1),
+  currentAmount:  z.number().min(1),
+  minInvestment:  z.number().min(1),
+  expectedReturn: z.string().min(1),
+  investors:      z.number().min(1),
+  status:         z.string().min(1)
 });
 
 export class Project extends BaseEntity {
@@ -26,5 +33,26 @@ export class Project extends BaseEntity {
   }
   get imageUrl(): string | null {
     return this._data.imageUrl;
+  }
+  get location(): string {
+    return this._data.location;
+  }
+  get targetAmount(): number {
+    return this._data.targetAmount;
+  }
+  get currentAmount(): number {
+    return this._data.currentAmount;
+  }
+  get minInvestment(): number {
+    return this._data.minInvestment;
+  }
+  get expectedReturn(): string {
+    return this._data.expectedReturn;
+  }
+  get investors(): number {
+    return this._data.investors;
+  }
+  get status(): string {
+    return this._data.status;
   }
 }
