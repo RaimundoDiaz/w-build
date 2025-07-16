e2a84f65662d03cf3352a4ee6e8155988bc602c2
RaimundoDiaz <rediazparada@gmail.com>
2025-07-16 19:30:58 -0400

    Add findAllByUserId method to ProjectUseCase for retrieving projects and investments associated with a specific user, improving data access and management.

--- src/core/useCases/models/Project.useCase.ts
+++ src/core/useCases/models/Project.useCase.ts
@@ -1,9 +1,14 @@
+import { Investment } from "@/core/entities/Investment.entity";
 import { Project } from "@/core/entities/Project.entity";
 import { ProjectsRepository } from "@/infrastructure/repositories/Project.repository";
 
 export abstract class ProjectUseCase {
   public static async findById(id: string): Promise<Project> {
     return await ProjectsRepository.findById(id);
+  }
+
+  public static async findAllByUserId(userId: string): Promise<{ projects: Project[], investments: Investment[] }> {
+    return await ProjectsRepository.findAllByUserId(userId);
   }
 
   public static async findAll(): Promise<Project[]> {
